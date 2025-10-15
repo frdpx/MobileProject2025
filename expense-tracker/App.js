50f5cb8ea655861cfaf18ae2b3b13442aa33a80e
srk-kh <sirikul.kh@ku.th>
2025-10-15 20:40:12 +0700

    up3

--- expense-tracker/App.js
+++ expense-tracker/App.js
@@ -1,7 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { RootNavigator } from "./src/navigation/RootNavigator";
+import {
+  registerTranslation,
+  en,
+  th,
+} from "react-native-paper-dates";
+
+registerTranslation("en", en);
+registerTranslation("th", th);
 
 export default function App() {
+  <StatusBar style="auto" />
   return <RootNavigator />;
 }
